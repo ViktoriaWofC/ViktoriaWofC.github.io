8e6543548567d3803f724ff63ac2953d1e7d81e7
ViktoriaWofC <vik-tori-s@yandex.ru>
2017-05-13 21:34:46 +0400

    ooo0

--- server.js
+++ server.js
@@ -1,16 +1,16 @@
-var http = require('https');
+var http = require('http');
 var Static = require('node-static');
-var WebSocketServer = new require('wss');
+var WebSocketServer = new require('ws');
 
 // подключенные клиенты
 var clients = {};
 
 // WebSocket-сервер на порту 8081
 var webSocketServer = new WebSocketServer.Server({port: 8081});
-webSocketServer.on('connection', function(wss) {
+webSocketServer.on('connection', function(ws) {
 
   var id = Math.random();
-  clients[id] = wss;
+  clients[id] = ws;
   console.log("новое соединение " + id);
 
   ws.on('message', function(message) {
